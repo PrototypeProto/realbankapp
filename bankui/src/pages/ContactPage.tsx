da8380da06aeabad592a2e88f949416083849b93
PrototypeProto <anunfoundx@gmail.com>
2026-07-16 16:35:58 -0400

    new ui pages to fluff site

---
+++ bankui/src/pages/ContactPage.tsx
@@ -0,0 +1,14 @@
+export function ContactPage() {
+	return (
+		<section className="page page--static">
+			<h1>Contact</h1>
+			<p>For general support, you can reach us through the channels below.</p>
+			<ul>
+				<li>Email: support@ginkoubank.com</li>
+				<li>Phone: +1 (555) 010-2450</li>
+				<li>Address: 123 Ginkou Drive, Suite 123, Orlando, FL</li>
+			</ul>
+			<p>Business hours: Monday to Friday, 9:00 AM to 9:00 PM.</p>
+		</section>
+	);
+}

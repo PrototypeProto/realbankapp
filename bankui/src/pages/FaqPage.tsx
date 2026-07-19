6543e3669609daa9aa1cd8a5291bc1eb2ab8d8fa
PrototypeProto <anunfoundx@gmail.com>
2026-07-18 20:20:57 -0400

    update faq page to include details on tech stack and add more details to faq

--- bankui/src/pages/FaqPage.tsx
+++ bankui/src/pages/FaqPage.tsx
@@ -7,7 +7,8 @@ export function FaqPage() {
 				<h2>What can I do in this app?</h2>
 				<p>
 					You can register, log in, view your accounts, transfer money between
-					accounts, and review transaction history.
+					accounts, and review transaction history. To open an account, an admin
+					must open an account for you.
 				</p>
 
 				<h2>Can admins do more?</h2>
@@ -19,6 +20,16 @@ export function FaqPage() {
 				<h2>Is this a production banking platform?</h2>
 				<p>
 					No. This project is intended for demonstration and educational use.
+				</p>
+
+				<h2>Tech stack?</h2>
+				<p>Backend: Python with FastAPI and served with Caddy</p>
+				<p>Database: MongoDB</p>
+				<p>Frontend: React + TypeScript, built using Vite</p>
+				<p>
+					Services used: AWS EC2 hosts the backend, S3 hosts the frontend files,
+					Cloudfront to serves the files publicly, and MongoDB Atlas for
+					database provider.
 				</p>
 			</div>
 		</section>
